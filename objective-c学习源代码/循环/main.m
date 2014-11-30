3741b09e1fa486d2abcf5837e7c3c9dfce384161
lcepy <xiangwenwe@foxmail.com>
2014-11-30 17:58:30 +0800

    Initial Commit

---
+++ objective-c学习源代码/循环/main.m
@@ -0,0 +1,9 @@
+//
+//  main.m
+//  循环
+//
+//  Created by xiangwenwen on 14/11/30.
+//
+//
+
+#import <Foundation/Foundation.h>
---
+++ objective-c学习源代码/循环/循环/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  循环
+//
+//  Created by xiangwenwen on 14/11/30.
+//  Copyright (c) 2014年 xiangwenwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
