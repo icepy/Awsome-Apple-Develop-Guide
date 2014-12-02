99faa3ff692f9998a19348d961c0223e10baa089
lcepy <xiangwenwe@foxmail.com>
2014-12-02 22:34:04 +0800

    Initial Commit

---
+++ objective-c/c/c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c
+//
+//  Created by xiangwenwen on 14/12/2.
+//  Copyright (c) 2014年 xiangwenwen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
