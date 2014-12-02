e37b4cbc50d8ce2d3b8f1c86288c55d415408af6
lcepy <xiangwenwe@foxmail.com>
2014-12-02 23:12:39 +0800

    添加c语言学习

---
+++ objective-c/c/c/ctrloutput.c
@@ -0,0 +1,20 @@
+//
+//  ctrloutput.c
+//  c
+//
+//  Created by xiangwenwen on 14/12/2.
+//  Copyright (c) 2014年 xiangwenwen. All rights reserved.
+//
+
+#include "ctrloutput.h"
+
+/*
+ 实现文件
+ 
+ 返回值类型  函数名  行参数（参数类型 参数名）
+ */
+
+int output(int a)
+{
+    return a*a;
+}
---
+++ objective-c/c/c/ctrloutput.h
@@ -0,0 +1,17 @@
+//
+//  ctrloutput.h
+//  c
+//
+//  Created by xiangwenwen on 14/12/2.
+//  Copyright (c) 2014年 xiangwenwen. All rights reserved.
+//
+
+#ifndef c_ctrloutput_h
+#define c_ctrloutput_h
+
+
+#endif
+
+//头文件 用于描述
+
+int output(int);
