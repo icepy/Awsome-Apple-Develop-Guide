aecc10435c23af20081710d2c4f6593e38b4adf3
lcepy <xiangwenwe@foxmail.com>
2014-12-07 19:42:49 +0800

    Initial Commit

---
+++ objective-c/Block/Block/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Block
+//
+//  Created by xiangwenwen on 14/12/7.
+//  Copyright (c) 2014年 xiangwenwen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
